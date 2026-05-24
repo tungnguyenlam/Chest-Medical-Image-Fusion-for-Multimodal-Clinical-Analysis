8c72df8c132b82f6c000906a60b3ef8786287be1
tungnguyenlam <nguyenlamtung2005@gmail.com>
2026-05-25 01:19:27 +0700

    Add AMD setup script

---
+++ scripts/set-up/set_up_amd.sh
@@ -0,0 +1,21 @@
+tmux
+
+curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+bash Miniforge3-$(uname)-$(uname -m).sh -b
+source miniforge3/bin/activate
+
+conda create -n camchex python=3.13 libjpeg-turbo -y
+conda activate camchex
+conda install -c conda-forge p7zip -y       # only needed to (un)bundle subsets
+
+pip install uv
+
+git clone https://github.com/tungnguyenlam/Chest-Medical-Image-Fusion-for-Multimodal-Clinical-Analysis.git
+cd Chest-Medical-Image-Fusion-for-Multimodal-Clinical-Analysis
+uv pip install torch torchvision --index-url https://download.pytorch.org/whl/rocm7.2
+uv pip install -r requirements.txt
+
+curl -fsSL https://herdr.dev/install.sh | sh
+export PATH="/root/.local/bin:$PATH"
+
+git submodule update --init --recursive mimic-cxr
